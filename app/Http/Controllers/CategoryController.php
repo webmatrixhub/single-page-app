e0af093561447b459acf299d9ffcbbf8918a42a9
Web-Matrix <webmatrixhub@gmail.com>
2018-08-08 16:39:01 +0530

    CURD for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,82 +4,86 @@ namespace App\Http\Controllers;
 
 use App\Model\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return Category::latest()->get();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    // Category ::creat($request->all());
+    $category = new Category();
+    $category->name = $request->name;
+    $category->slug = str_slug($request->name);
+    $category->save();
+    return response('Created', Response::HTTP_CREATED);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Model\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Category $category)
+  {
+    return $category;
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Model\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Category $category)
+  {
         //
-    }
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Model\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Category $category)
+  {
+    $category->update(
+      [
+        'name' => $request->name,
+        'slug' => str_slug($request->name)
+      ]
+    );
+    return response('Updated', Response::HTTP_ACCEPTED);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Model\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Category $category)
+  {
+    $category->delete();
+    return response('Deleted', Response::HTTP_NO_CONTENT);
+  }
 }
