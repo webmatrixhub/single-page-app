a3c9d9d3f58ca6cf1c5f190fbb9646b97ca3a1c6
Web-Matrix <webmatrixhub@gmail.com>
2018-08-08 14:13:02 +0530

    curd for question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,13 @@ use App\User;
 
 class Question extends Model
 {
+  // protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+  protected $guarded = [];
+
+  public function getRouteKeyName()
+  {
+    return 'slug';
+  }
   public function user()
   {
     return $this->belongsTo(User::class);
@@ -18,5 +25,9 @@ class Question extends Model
   public function category()
   {
     return $this->belongsTo(Category::class);
+  }
+  public function getPathAttribute()
+  {
+    return asset("api/question/$this->slug");
   }
 }
