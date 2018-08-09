7ab8f93095a136c14c9aa39bb134e05e6a25b50d
Web-Matrix <webmatrixhub@gmail.com>
2018-08-09 13:54:45 +0530

    Like and Dislike System for Reply

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 
+
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
