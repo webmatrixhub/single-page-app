2a8e4ae41608861512467ef3fc88e99f0c60f746
Web-Matrix <webmatrixhub@gmail.com>
2018-08-09 12:01:42 +0530

    CURD for reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,83 +3,70 @@
 namespace App\Http\Controllers;
 
 use App\Model\Reply;
+use App\Model\Question;
+use App\Http\Resources\QuestionResource;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\ReplyResource;
 
 class ReplyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Question $question)
+  {
+    return ReplyResource::collection($question->replies);
+    // return Reply::latest()->get();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Question $question, Request $request)
+  {
+    $reply = $question->replies()->create($request->all());
+    return response(['reply' => new ReplyResource($reply)], Response::HTTP_CREATED);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Model\Reply  $reply
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Question $question, Reply $reply)
+  {
+    return new ReplyResource($reply);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reply $reply)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Model\Reply  $reply
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Question $question, Request $request, Reply $reply)
+  {
+    $reply->update($request->all());
+    return response('Updated', Response::HTTP_ACCEPTED);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reply $reply)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Reply $reply)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Reply $reply)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Model\Reply  $reply
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Question $question, Reply $reply)
+  {
+    $reply->delete();
+    return response(null, Response::HTTP_NO_CONTENT);
+  }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,6 +7,7 @@ use App\User;
 
 class Reply extends Model
 {
+  protected $guarded = [];
   public function question()
   {
     return $this->belongsTo(Question::class);
