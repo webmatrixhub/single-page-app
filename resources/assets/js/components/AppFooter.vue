7099e63bb6c133b5d6c28300c63f631bd95933da
Web-Matrix <webmatrixhub@gmail.com>
2018-08-16 14:01:10 +0530

    vuitify, Vue Router and initial component

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer class="pa-3">
+    Web-Matrix
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
